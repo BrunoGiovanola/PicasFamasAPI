b4f7a03c1ac7de41dbdf98ee164ba0644cde6842
Bruno Giovanola <bruno.giovanola@mi.unc.edu.ar>
2026-07-02 15:37:28 -0300

    Implementación de endpoints base del juego

--- NumberGuessGameApi/Migrations/20260702183441_AddIsFinishedToGame.Designer.cs
+++ NumberGuessGameApi/Migrations/20260702183441_AddIsFinishedToGame.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NumberGuessGameApi.Data;
 
@@ -11,9 +12,11 @@ using NumberGuessGameApi.Data;
 namespace NumberGuessGameApi.Migrations
 {
     [DbContext(typeof(GameDbContext))]
-    partial class GameDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260702183441_AddIsFinishedToGame")]
+    partial class AddIsFinishedToGame
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ NumberGuessGameApi/Migrations/20260702183441_AddIsFinishedToGame.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NumberGuessGameApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsFinishedToGame : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Finished",
+                table: "Games",
+                newName: "IsFinished");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsFinished",
+                table: "Games",
+                newName: "Finished");
+        }
+    }
+}
